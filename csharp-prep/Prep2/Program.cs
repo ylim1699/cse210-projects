24828822742ed79847903769a14db530caedd887
ylim1699 <ylim.0348@gmail.com>
2025-01-11 22:34:45 -0700

    prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,42 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("What is your grade percentage?");
+        int grade = int.Parse(Console.ReadLine());
+
+        string letter = " ";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congrats! You're passing your class!");
+        }
+        else
+        {
+            Console.WriteLine("You can do better...");
+        }
     }
 }
