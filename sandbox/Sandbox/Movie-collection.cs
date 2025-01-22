1c66a2e17f113fcd1f0b34567c7393b460df6f85
ylim1699 <ylim.0348@gmail.com>
2025-01-21 20:42:50 -0700

    class practice

---
+++ sandbox/Sandbox/Movie-collection.cs
@@ -0,0 +1,6 @@
+public class ManyMovie
+{
+    public string _name;
+    public Movie _movie1 = new Movie();
+    public Movie _movie2 = new Movie();
+}
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.IO.Compression;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!!!!");
+        Movie movie1 = new Movie();
+        Movie movie2 = new Movie();
+
+        movie1._title = "All of us are dead";
+        movie1._year = 2022;
+        movie1._runTime = 72;
+        movie1._rating = "PG-13";
+
+        movie1.DisplayTitle();
+        movie1.DisplayInfo();
+        Console.WriteLine("");
+
+        movie2._title = "Train to Busan";
+        movie2._year = 2018;
+        movie2._runTime = 80;
+        movie2._rating = "PG-13";
+
+        movie2.DisplayTitle(); 
+        movie2.DisplayInfo();
+
+        ManyMovie owner = new ManyMovie();
+        owner._name = "Yujin Lim";
+
+        Console.WriteLine(owner._movie1._title);
+        
     }
+
 }
