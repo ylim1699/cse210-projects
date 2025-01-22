73859c107e0c8702a3dfaabf5261f0a661034061
ylim1699 <ylim.0348@gmail.com>
2025-01-21 21:14:47 -0700

    Finished prep week 3

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        Job job2 = new Job();
+        Resume resume = new Resume();
+        resume._name = "yujin Lim";
+
+        job1._company = "Microsoft";
+        job1._jobTitle = "software engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+        
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+        
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
+
     }
 }
