863302564d66f26fc19f618633664dbe698ac1d6
ylim1699 <ylim.0348@gmail.com>
2025-02-04 23:27:43 -0700

    finished prepare week 5

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetTop());
+            
+        Console.WriteLine(f1.GetFractionString());
+            
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetTop());
+            
+
+        Fraction f3 = new Fraction(6, 7);
+        Console.WriteLine(f3.GetDecimalValue());
+
     }
 }
